5c149c946df3457ed0c7104b6a3dcceb52fa89d6
LucasDiablo <christopherlucasphillips@gmail.com>
2017-02-03 11:14:39 -0500

    Initial Commit

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 2/1/17
+ * Time: 4:20 PM
+ */
---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 2/1/17
+ * Time: 4:21 PM
+ */
